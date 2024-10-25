880ece4ba770b7c3ccd035d3670d893e7f63876b
Thinh <Thinh@DESKTOP-RS2T3UO>
2024-10-25 14:51:15 +0700

    comit giao dien category

--- projectTMDT/src/main/java/com/TMDT/controllers/admin/CategoryController.java
+++ projectTMDT/src/main/java/com/TMDT/controllers/admin/CategoryController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("admin/category")
+
 public class CategoryController {
-	@GetMapping
+	@RequestMapping("admin/category")
 	public String index() {
 		return "admin/category/index";
+	}
+	
+	@RequestMapping("admin/add-category")
+	public String add() {
+		return "admin/category/add";
 	}
 }
