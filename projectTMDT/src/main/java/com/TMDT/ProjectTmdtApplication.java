d8adf652b84ad42fe9d07c88ae75980369df673a
Thinh <Thinh@DESKTOP-RS2T3UO>
2024-10-25 14:13:40 +0700

    commit Project

---
+++ projectTMDT/src/main/java/com/TMDT/ProjectTmdtApplication.java
@@ -0,0 +1,13 @@
+package com.TMDT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTmdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTmdtApplication.class, args);
+	}
+
+}
